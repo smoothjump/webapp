0a69bb019a1ab132e5e38d3a684e85e65796870d
smoothjump <smoothjump@gmail.com>
2016-10-10 18:44:31 +0800

    updated

---
+++ init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `login_name` varchar(20) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `password` varchar(12) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_lnm` (`login_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8
